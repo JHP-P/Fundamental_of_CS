34e63b569c0c402e895e46829197076ce8731c28
JHP <163867825+JHP-P@users.noreply.github.com>
2024-08-19 22:48:07 +0900

    Delete ch5_pointer directory

--- ch5_pointer/LinkedStack.h
+++
@@ -1,9 +0,0 @@
-// LinkedStack.h : 연결리스트로 구현된 스택클래스 (연결된 스택 클래스 구현)
-#include "Node.h"   
-class LinkedStack {
-    Node*   top;
-public:
-    LinkedStack()   { top = NULL; }
-    ~LinkedStack()  { while( !isEmpty()) delete pop(); }
-    
-}
